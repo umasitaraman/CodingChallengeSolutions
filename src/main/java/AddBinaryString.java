55c6690ffb69e2e35a9bdc8ef24fec577e6a54dc
umasitaraman <36041143+umasitaraman@users.noreply.github.com>
2022-05-16 20:02:24 -0400

    Added and Updated Few more LeetCode Challenge solutions

---
+++ src/main/java/AddBinaryString.java
@@ -0,0 +1,33 @@
+//Avoid using built-in big integers to solve this challenge.
+//Implement them yourself, since this is what you would be asked to do during a real interview.
+//Given two binary strings a and b, add them together and return the resulting string.
+
+
+public class AddBinaryString {
+    String solution(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        StringBuilder str = new StringBuilder();
+        int carry = 0;
+        int sum = 0;
+        while (i >= 0 || j >= 0) {
+            int digitA = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int digitB = (j >= 0) ? b.charAt(j) - '0' : 0;
+
+            sum = digitA + digitB + carry;
+            String c = String.valueOf(sum % 2);
+            str.append(c);
+            carry = sum / 2;
+            i--;
+            j--;
+        }
+
+        if (carry != 0) str.append("1");
+        return str.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        AddBinaryString abs = new AddBinaryString();
+        System.out.println(abs.solution("1010", "1011"));
+    }
+}
